28368025eb2edf5b732517a0332d07781723efed
kavitachauhan <chauhan.kavita@hotmail.com>
2024-04-11 21:21:54 +0530

    Revert "Security"
    This reverts commit e0381f563a01ae10bf745303856f0d5511292e11.

--- src/main/java/com/example/SpringSecurity/repo/IAppUserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringSecurity.repo;
-
-import com.example.SpringSecurity.entity.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAppUserRepo extends JpaRepository<AppUser, Long> {
-    AppUser findByUsername(String username);
-}
